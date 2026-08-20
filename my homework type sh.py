d77e6f8442bc40cca52636815e7b68e79ec15c0f
hesbi <mm.chllenge@gmail.com>
2026-08-20 03:02:47 -0700

    3RD homework (ngl i seem to be getting into this)

--- my homework type sh.py
+++ my homework type sh.py
@@ -1,59 +1,65 @@
-name_cm = input("plz enter ur name and 'code meli' : ")
-name = name_cm[ : -10]
-CM = int(name_cm[-10 : ])
-print('name = ' , name)
-print('code meli = ' , CM)
-#--------------------------------------------------------
-phone_number = input("plz enter ur phone number: ")
-PH = phone_number[ 1 : 4]
-print("operator thingy = " , PH)
-#--------------------------------------------------------
-username = input('plz enter ur username : ')
-first_L = username[0]
-USER = username.index(" ") #could've used find() to but whatever -_-
-print("username = " , first_L + "." + username[USER + 1 : ])
-#--------------------------------------------------------
-cc_number = input('plz enter ur credit card number : ') #cc stand for credit card btw
-reverse_cc = cc_number[: :-1]
-print(reverse_cc)
-#--------------------------------------------------------
-traveled_distance = int(input('plz enter the travel distance: '))
-if traveled_distance <= 2000 :
-    print("payment =" , 5000)
-if traveled_distance >= 7000 : 
-    pay = (traveled_distance) // 5000
-    payment = 2000 + (5000*pay) 
-    print("payment =" , payment)
-#--------------------------------------------------------
-price_tag = int(input('plz enter the price tag of the object u bought: '))
-if price_tag < 500000 :
-    payment = price_tag
-    print("payment =" , payment)
-if 500000 <= price_tag <= 1000000 :
-    payment = price_tag // (10/100)
-    print("payment =" , payment)
-if price_tag > 1000000 :
-    payment = price_tag // (15/100)
-    print("payment =" , payment)
-#--------------------------------------------------------
-credit_card_number = input('plz enter ur credit card number: ')
-bank_sepah = 6037
-if credit_card_number[ : 5] == bank_sepah :
-    print( "credit card is from bank_sepah" )
-else : 
-    print('your credit card =' , credit_card_number)
-#--------------------------------------------------------
-time = float(input('give me a time [0|24]: '))
-if time < 0 or 25 <= time :
-    print('the time u entered is out of bound')
-if 0 <= time <= 6 : 
-    print('its midnight')
-if 6 < time <= 12 :
-    print('its day time')
-if 12 < time <= 18 :
-    print('its afternoon')
-if 18 < time <= 24 :
-    print(' its night time')
+import random  
+number = random.randint(0 , 101)
+print('ay twin welcome to my "number guessing game" a random number has been selected dont ask how just try to guess it u got as many tries as u want goodluck')
+while True :
+    guess = input("guess ur number: ")
+    if guess.isdigit() :
+        guess = int(guess)
+        if guess > number :
+            print('ur  number has a higher value then the choosen number')
+        elif guess < number :
+            print('ur number has alower value then the choosen number')
+        elif guess == number :
+            print('congrats! u won chief')
+            break
+    else :
+        print("u didnt enter a number brochacho")
+#---------------------------------------------------------------------
+import random
+RPS = ["rock" , 'paper' , 'scissors']
+system = random.choice(RPS) 
+print('welcome to my rock paper scissors game')
+while True :
+    user = input('alr, [rock - paper - scissors] : ').lower().strip()
+    print('system has picked =' , system)
+    if system == user :
+        print('tied')
+    elif system == 'rock' and user == 'paper' :
+        print('u win!')
+        break
+    elif system == 'paper' and user == 'scissors':
+        print('u win!')
+        break
+    elif system == 'scissors' and user == 'rock':
+        print('u win!')
+        break
+    else :
+        print('u lost! *imagine losing to a robot lmao*')
+        break
+#--------------------------------------------------------------
+while True :
+    print('password must contain atleast 8 letters from the alphabet and the last piece of must contain a number')
+    password = input('enter ur password :')
+    if len(password) == 8 and not password[0:4].isdigit() and password[7].isdigit() :
+        print('valid password!')
+        break
+    else :
+        print('invalid passsword. . .')
+#--------------------------------------------------------------
+counter = 0
+while True :
+    num = (int(input('enter a number (keep in mind if u enter the number zero u dont ahve too enter a number anymore): ')))
+    counter += num
+    if num == 0 :
+        print('sum of all the numbers u put in :' , counter)
+        break
+#--------------------------------------------------------------
+for i in range(0 , 6) :
+    print()
+    for a in range(0,i+1) :
+        print('*' , end="")
+
+
 
 
 
